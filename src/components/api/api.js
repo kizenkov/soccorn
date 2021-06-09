b750b0324285c71f0e2eba34858ced4b874a6246
kizenkov <kizenkov@tut.by>
2021-06-09 23:39:03 +0300

    refactoring commit

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true,
     headers: {
-        'API-KEY': 'f7609989-8524-45ae-bbba-10ed530a756b'
+        'API-KEY': '669fbc17-8ab9-40b1-8b82-54cc83790bf8'
     }
 })
 
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import store from './store/reduxStore';
 import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root')
 )
 
