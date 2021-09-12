ebc1ed24ce7945ff359caf661714cf89ddb77339
kizenkov <kizenkov@tut.by>
2021-09-12 23:39:00 +0300

    Update Loading.jsx

--- src/common/Loading.jsx
+++ src/common/Loading.jsx
@@ -3,7 +3,7 @@ import classes from './Loading.module.css'
 
 const Loading = () => {
     return <div className={classes.loading}>
-        <img src={loading} alt={loading}/>
+//         <img src={loading} alt={loading}/>
     </div>
 }
 
